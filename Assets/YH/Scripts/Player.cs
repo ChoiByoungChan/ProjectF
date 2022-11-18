62392c61083fb364447f329dd673738f4b49b598
HanYoungHyeon <112858988+HanYoungHyeon@users.noreply.github.com>
2022-11-18 18:07:05 +0900

    Fix : UI
    UI 해상도 조정

--- Assets/YH/Scripts/Player.cs
+++ Assets/YH/Scripts/Player.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.TextCore.Text;
-
-
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour,IHitable
@@ -14,14 +11,12 @@ public class Player : MonoBehaviour,IHitable
     private Slider hpBar;
     [SerializeField]
     private TextMeshProUGUI hpText;
-    [SerializeField] private GameObject gameOverButton;
     private CharacterController character;
     public GameObject comboUI;
     public ParticleSystem swordEffect;
     public ParticleSystem shieldEffect;
     public ParticleSystem hitEffect;
-    public float moveSpeed;
-    public TurnWaveLight waveAction;
+    private float moveSpeed;
     public float comboNumber;
     public float maxhp;
     public float minAtk;
@@ -33,35 +28,45 @@ public class Player : MonoBehaviour,IHitable
     public bool isAttackOver;
     public bool isGuardOver;
     public Vector3 moveInput;
+    public Vector3 movePosition;
     private Vector3 gravity;
     public JoyStick joyStick;
-    public Animator animator;
-    public Vector3 movePosition;
+    
     private IStater curState;
-    public PlayerIdleState playerIdleState;
+    private PlayerIdleState playerIdleState;
     private PlayerWalkState playerWalkState;
     private PlayerAttackState playerAttackState;
-    public PlayerAttackTwoState playerAttackTwoState;
+    private PlayerAttackTwoState playerAttackTwoState;
     private PlayerShieldState playerShieldState;
     private PlayerRollState playerRollState;
     private PlayerHitState playerHitState;
     private PlayerDieState playerDieState;
     private WaitForSeconds fiveSeconds;
-    public PlayerAttackThreeState playerAttackThreeState;
     private float hp;
     public float Hp
     {
         get { return hp; }
         set 
         {
+            if (isGuardOver == false)
+                return;
+            if (hp < maxhp && isHit == false && value < 0)
             {
-                hp = value;
-                HpUI();
+                hp = value / def;
+                hpBar.value = hp;
+                hpText.text = "HP : "+ hp;
+                isHit = true;
+                SetState(playerHitState);
                 if (hp <= 0)
                 {
                     SetState(playerDieState);
-                    //hp = maxhp;
+                    hp = maxhp;
                 }
+            }
+            if(value > 0)
+            {
+                hp = value;
+                hpBar.value = hp;
             }
         }
     }
@@ -94,7 +99,6 @@ public class Player : MonoBehaviour,IHitable
     }
     private void Awake()
     {
-        animator = GetComponent<Animator>();
         comboNumber = 0;
         maxhp = 100;
         hp = maxhp;
@@ -118,13 +122,11 @@ public class Player : MonoBehaviour,IHitable
         playerHitState = new PlayerHitState(this.gameObject);
         playerDieState = new PlayerDieState(this.gameObject);
         playerRollState = new PlayerRollState(this.gameObject);
-        playerAttackThreeState = new PlayerAttackThreeState(this.gameObject);
     }
     private void Start()
     {
         SetState(playerIdleState);
         StartCoroutine(HpRecovery());
-        waveAction.NextWaveAction += NextWaveSetPosition;
     }
     private void Update()
     {
@@ -137,16 +139,10 @@ public class Player : MonoBehaviour,IHitable
     {
         if (isAttackOver)
         {
-            movePosition = transform.InverseTransformDirection(inputDirection);
+            movePosition = transform.InverseTransformDirection(-inputDirection);
             character.Move(movePosition * moveSpeed * Time.deltaTime);
             SetState(playerWalkState);
         }
-    }
-    private void NextWaveSetPosition()
-    {
-        character.enabled = false;
-        transform.position = new Vector3(-0.5f, 6.05f, 21.55f);
-        character.enabled = true;
     }
     public void Attack()
     {
@@ -183,24 +179,7 @@ public class Player : MonoBehaviour,IHitable
     }
     public void Hit(float damage)
     {
-        if (isGuardOver || !isHit)
-        {
-            Hp -= (damage / Def);
-            isHit = true;
-            SetState(playerHitState);
-        }
-    }
-    private void HpUI()
-    {
-        hpBar.maxValue = maxhp;
-        hpBar.value = hp;
-        hpText.text = "HP : " + hp;
-    }
-
-    public void PlayerDie()
-    {
-        Time.timeScale = 0.1f;
-        gameOverButton.SetActive(true);
+        Hp -= damage;
     }
 }
 
